3da4a84d4bdd55d2fa3a4356ecb4fbb8c556336e
Björn <Bjorn.oudkerk@outlook.com>
2020-10-26 15:05:41 +0100

    Added a Database and set-up AJAX and JSON intergration

--- html/16myCards/script.js
+++ html/16myCards/script.js
@@ -9,24 +9,50 @@ canvas.height = height;
 
 // begin hier met jouw code voor deze opdracht
 
-let img,numberOnARow,numberOnAColomn,sx,sy,sw,sh,x,y,w,h,index;
+let img,numberOnARow,numberOnAColomn,sx,sy,sw,sh,x,y,w,h,index,htmlstring;
+
+var cardDescription = document.getElementById('animal-info');
+var btn = document.getElementById("btn");
 
 img = new Image();
-img.src = "images/cardDeck.png"
+img.src = "images/deck.jpg"
 
-numberOnARow = 13;
-numberOnAColomn = 5;
+numberOnARow = 10;
+numberOnAColomn = 7;
 
-index = Math.floor(Math.random()*52);
+
+btn.addEventListener("click", function() {
+  index = Math.floor(Math.random()*64);
+  var ourReqeust = new XMLHttpRequest();
+  ourReqeust.open('GET', 'Cards.json');
+  // Use POST if you want to send data
+
+  ourReqeust.onload = function(){
+    var ourData = JSON.parse(ourReqeust.responseText);
+    renderHTML(ourData);
+  };
+  ourReqeust.send();
+});
 
 img.addEventListener('load', ()=>{
   sw = img.width/numberOnARow;
   sh = img.height/numberOnAColomn;
   anime();
+  console.log(index);
 })
+
+function renderHTML(data)
+{
+  var htmlString = "";
+
+  htmlString = "<p>" + "This is a " + data[index].CardType + " named: " + data[index].CardName + ".";
+
+  cardDescription.insertAdjacentHTML('beforeend', htmlString);
+}
+
 
 function anime(){
   sx=(index%numberOnARow)*sw;
   sy = Math.floor(index/numberOnARow)*sh;
-  context.drawImage(img,sx,sy,sw,sh,width/2,height/2,sw,sh)
+  context.drawImage(img,sx,sy,sw,sh,sw*2.5,0,sw,sh)
 }
